eb1a5d773253dee0be1a101f7296954515a4dcef
Jason  Elgin <jason@Jasons-MBP.local>
2017-08-18 14:31:34 -0400

    fix missing close paren

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -29,7 +29,7 @@ function loadConfig() {
 
 // Build the "dist" folder by running all of the below tasks
 gulp.task('build',
- gulp.series(clean, gulp.parallel(pages, sass, javascript, images, copy));
+ gulp.series(clean, gulp.parallel(pages, sass, javascript, images, copy)));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
